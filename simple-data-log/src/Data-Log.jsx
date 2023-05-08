090d8437b4c11dde3439b7e65b9d58b3dc7e592c
Cameron McConnell <cameron@angry-dogman.com>
2023-05-08 16:26:21 -0700

    Finished the simple-data-log.
    This application takes a first and last name as input and can dislay
    all first names that share a common last name. This is done by using
    express.js to store the data within an object/map that stores the first
    names into lists that correspond the last name keys. There is no data base
    but I will soon create another project that will take in more complex data and
    will use MongoDB as a backend.

--- simple-data-log/src/Data-Log.jsx
+++ simple-data-log/src/Data-Log.jsx
@@ -2,24 +2,48 @@ import './Data-Log.css'
 import {useState} from 'react';
 
 export default function Data_Log() {
-    //const [data, setData] = useState([]);
+    
+    // render first names
+    const [data, setData] = useState([]);
+
+    // save request last name
     const [reqL, setReqL] = useState('');
 
+    // save submit first and last names
+    const [subF, setSubF] = useState('');
+    const [subL, setSubL] = useState('');
+
+    // handle requests to render list of first names paired to reqL
     function handleReq(event) {
         event.preventDefault();
-        const send = reqL;
+        let obj = {};
+        obj[reqL] = '';
+        setReqL('');
         fetch('http://localhost:5000/request', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(send)
+            body: JSON.stringify(obj)
         })
-        .then(res => res.json)
-        .then(send => console.log(send))
+        .then(res => res.json())
+        .then(send => setData(send))
         .catch(error => console.error(error));
     }
 
-    function handleChange(event) {
-        setReqL(event.target.value);
+    // handle submission of subL and subF pair
+    function handleSub(event) {
+        event.preventDefault();
+        let obj = {};
+        obj[subL] = subF;
+        setSubL('');
+        setSubF('');
+        fetch('http://localhost:5000/submit', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(obj)
+        })
+        .then(res => res.text())
+        .then(sub => console.log(sub))
+        .catch(err => console.error(err));
     }
 
     return (
@@ -27,22 +51,31 @@ export default function Data_Log() {
             <div className="inner-container">
                 <h1>Submit Data to Server</h1>
                 <hr></hr>
-                <form className="form-container" action="http://localhost:5000/" method="POST">
-                    <label for="fname">First Name:</label>
-                    <input type="text" name="fname"></input>
-                    <label for="lname">Last Name:</label>
-                    <input type="text" name="lname"></input>
+                <form className="form-container" onSubmit={(event) => handleSub(event)}>
+                    <label>First Name:</label>
+                    <input type="text" value={subF} onChange={(event) => setSubF(event.target.value)}></input>
+                    <label>Last Name:</label>
+                    <input type="text" value={subL} onChange={(event) => setSubL(event.target.value)}></input>
                     <input type="submit"></input>
                 </form>
             </div>
             <div className="inner-container">
                 <h1>Request Data from Server</h1>
                 <hr></hr>
-                <form className="form-container" onSubmit={() => handleReq}>
-                    <label for="lname">Last Name:</label>
-                    <input type="text" name="lname" value={reqL} onChange={() => handleChange}></input>
+                <form className="form-container" onSubmit={(event) => handleReq(event)}>
+                    <label>Last Name:</label>
+                    <input type="text" value={reqL} onChange={(event) => setReqL(event.target.value)}></input>
                     <input type="submit"></input>
                 </form>
+                <h2>First Names:</h2>
+                <span id="hr-span">
+                    <hr style={{width: "35%"}}></hr>
+                </span>
+                <ul>
+                    {data.map((d, ind) => (
+                        <li key={ind}>{d}</li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
--- simple-data-log/src/server.js
+++ simple-data-log/src/server.js
@@ -1,20 +1,35 @@
+
+// define express app and middleware
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
 
+// storage
 let data = {}
 
+// app middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// CORS headers
+app.use((_, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+// open port
 app.listen(port, () => {
     console.log('Listening on port %d', port);
 });
 
-app.post('/', (req, res) => {
-    const lname = req.body.lname;
-    const fname = req.body.fname;
+// handle submit requests
+app.post('/submit', (req, res) => {
+    
+    // get submit json data
+    const lname = Object.keys(req.body)[0];
+    const fname = req.body[lname];
     
     if (lname in data) {
         data[lname].push(fname);
@@ -22,15 +37,21 @@ app.post('/', (req, res) => {
     else {
         data[lname] = [fname];
     }
+    
+    res.type('text/plain');
+    res.send('Submitted Successfully');
 });
 
-/*app.post('/request', (req, res) => {
-    const lname = req.body.lname;
+// handle request requests
+app.post('/request', (req, res) => {
+    
+    // get submit json data
+    const lname = Object.keys(req.body)[0];
 
     if (lname in data) {
-        res.send(data[lname])
+        res.send(data[lname]);
     }
     else {
-        res.send("No first names with this last name.")
+        res.send([]);
     }
-});*/
+});
